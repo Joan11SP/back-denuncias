f6939cbc8bf0fdae8c350e62f7b2c7cd7d997dd0
Joan11SP <sebaspe12345@gmail.com>
2021-03-09 06:39:35 +0100

    mongodb en la nube

--- src/Database/mongodb.js
+++ src/Database/mongodb.js
@@ -6,14 +6,14 @@ var opciones = {
     useNewUrlParser:true,
     useUnifiedTopology:true
 }
-/*mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Certificado?retryWrites=true&w=majority',opciones)
+mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Denuncias?retryWrites=true&w=majority',opciones)
     .then(db => console.log('conected to Mongo'))
-    .catch(err => console.error(err)); */
+    .catch(err => console.error(err)); 
 let port = config_variables.port_db;
 let db = config_variables.nombre_db;
 let host = config_variables.hostname
-mongoose.connect(`mongodb://${host}:${port}/${db}`, opciones)
+/*mongoose.connect(`mongodb://${host}:${port}/${db}`, opciones)
     .then(db => console.log('conected to Mongodb'))
-    .catch(err => console.error(err));;
+    .catch(err => console.error(err));;*/
 
 module.exports = mongoose
